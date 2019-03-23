3b439ed4ce906cb89962c7f9d20888e19a9ff13b
simonalong <simonalongs@outlook.com>
2019-03-23 14:07:19 +0800

    fix 修复测试的表结构

--- src/test/resources/db/test.sql
+++ src/test/resources/db/test.sql
@@ -1,3 +1,4 @@
+CREATE TABLE `neo_table1` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `group` char(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '数据来源组，外键关联lk_config_group',
   `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '任务name',
@@ -5,4 +6,4 @@
   `age` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `group_index` (`group`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
