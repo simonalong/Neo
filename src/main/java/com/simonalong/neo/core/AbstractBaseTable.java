eaf995e65d34cd310b5dfc73bd8b092e73ac391c
simonalong <simonalong@126.com>
2020-07-15 10:09:55 +0800

    chg 修改values的处理方式

--- src/main/java/com/simonalong/neo/core/AbstractBaseTable.java
+++ src/main/java/com/simonalong/neo/core/AbstractBaseTable.java
@@ -226,12 +226,12 @@ public abstract class AbstractBaseTable extends AbstractTableAsync implements Ta
 
     @Override
     public <T> List<T> values(Class<T> tClass, String field, NeoMap searchMap){
-        return getDbInner().values(getTableName(), tClass, field, searchMap);
+        return getDbInner().values(tClass, getTableName(), field, searchMap);
     }
 
     @Override
     public <T> List<T> values(Class<T> tClass, String field, Object entity){
-        return getDbInner().values(getTableName(), tClass, field, entity);
+        return getDbInner().values(tClass, getTableName(), field, entity);
     }
 
     @Override
